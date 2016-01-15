79a8ccfbcff11534fb850b49c6ab30d91871c678
Ricardo Huamani <drihupp@gmail.com>
2016-01-15 23:32:57 +0000

    Agregado lodash

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 var express = require('express');
 var app = express();
+var faker = require('faker');
 
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.send('<p>Reto Backend 1</p><p><a href="/link-1">Link 1</a></p><p><a href="/link-2">Link 2</a></p>');
 });
 
 app.listen(3000, function () {
