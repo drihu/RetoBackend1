2021f80e931705ea659a32cedd393d54bd611324
Ricardo Huamani <drihupp@gmail.com>
2016-01-15 23:35:27 +0000

    Require lodash

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 var faker = require('faker');
+var _ = require('lodash');
 
 app.get('/', function (req, res) {
   res.send('<p>Reto Backend 1</p><p><a href="/link-1">Link 1</a></p><p><a href="/link-2">Link 2</a></p>');
