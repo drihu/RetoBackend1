788a07cc1cfc8e89858ac271552921f2c641f609
Ricardo Huamani <drihupp@gmail.com>
2016-01-16 00:13:57 +0000

    Reto Backend Semana 1

--- index.js
+++ index.js
@@ -1,10 +1,52 @@
 var express = require('express');
 var app = express();
 var faker = require('faker');
+faker.locale = 'es';
 var _ = require('lodash');
 
+var generarIndependiente = function () {
+    var randomName = faker.name.findName();
+    var randomEmail = faker.internet.email();
+    var randomPhone = faker.phone.phoneNumber();
+    var randomCity = faker.address.city();
+
+    return {
+        nombre: randomName,
+        email: randomEmail,
+        telefono: randomPhone,
+        ciudad: randomCity
+    }
+}
+
+var generarEmpresa = function () {
+    var randomCompanyName = faker.company.companyName();
+    var randomAccount = faker.finance.account();
+    var randomAccountName = faker.finance.accountName();
+    var randomCommerce = faker.commerce.department();
+
+    return {
+        empresa: randomCompanyName,
+        numeroDeCuenta: randomAccount,
+        tipoDeCuenta: randomAccountName,
+        departamento: randomCommerce
+    }
+}
+
+
 app.get('/', function (req, res) {
-  res.send('<p>Reto Backend 1</p><p><a href="/link-1">Link 1</a></p><p><a href="/link-2">Link 2</a></p>');
+    res.send('<p>Reto Backend 1</p><p><a href="/freeters">Freeters</a></p><p><a href="/companies">Companies</a></p>');
+});
+
+app.get('/freeters', function (req, res) {
+    var cantidad = _.random(10,15);
+    var usuarios = _.times(cantidad, generarIndependiente);
+    res.json(usuarios);
+})
+
+app.get('/companies', function (req, res) {
+    var cantidad = _.random(5, 10);
+    var companies = _.times(cantidad, generarEmpresa);
+    res.json(companies);
 });
 
 app.listen(3000, function () {
